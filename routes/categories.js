f56ca0533103bb94592aa21633bfc80d1955b629
zakaria ben salek <zakariaazaraf@gmail.com>
2021-03-05 18:15:33 +0100

    finish categorie Router

--- routes/categories.js
+++ routes/categories.js
@@ -15,6 +15,26 @@ router.get('/', async (req, res)=>{
     }
 })
 
+router.get('/:id', async (req, res)=>{
+    const {id} = req.params
+    try{
+        const categorie = await Categorie.findById(id)
+        if(categorie){
+            res.status(200).json({
+                categorie: categorie
+            })
+            return
+        }
+        res.status(201).json({
+            message: 'There id no Such Categorie Id'
+        })
+    }catch{
+        res.status(500).json({
+            message: 'Failed Getting Categorie'
+        })
+    }
+})
+
 router.post('/', async (req, res)=>{
     const {name, description} = req.body
     const categorie = new Categorie({
@@ -32,6 +52,50 @@ router.post('/', async (req, res)=>{
     }catch{
         res.status(500).json({
             message: 'Failed To Create Categorie'
+        })
+    }
+})
+
+router.put('/:id', async (req, res)=>{
+    const {id} = req.params
+    try{
+        const categorie = await Categorie.findById(id)
+        if(categorie){
+            const {name, description} = req.body
+            categorie.name = name || categorie.name
+            categorie.description = description || categorie.description
+            try{
+                const categorieUpdated = await categorie.save()
+                res.status(200).json({
+                    categorie: categorieUpdated
+                })
+            }catch{
+                res.status(500).json({
+                    message: 'Failed Updaiting Categorie'
+                })
+            }
+            return
+        }
+        res.status(201).json({
+            message: 'There Is No Such Categorie With This ID'
+        })
+    }catch{
+        res.status(500).json({
+            messag: 'Trouble Getting Categorie'
+        })
+    }
+})
+
+router.delete('/:id', async (req, res)=>{
+    const {id} = req.params
+    try{
+        const categorieDeleted = await Categorie.findById(id).remove()
+        res.status(200).json({
+            message: 'Categorie Deleted Successfully'
+        })
+    }catch{
+        res.status(500).json({
+            message: 'Failed Deleting Categorie'
         })
     }
 })
