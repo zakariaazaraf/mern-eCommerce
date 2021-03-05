6272aaf0402d940541540ba31d6c77093da9ded9
zakaria ben salek <zakariaazaraf@gmail.com>
2021-03-05 13:48:30 +0100

    finish user router

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema =  mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    dateJoined: {type: Date, required: true, defaule: Date.now()}
 })
 
 module.exports = mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(methodOverride('_method'))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+app.use(bodyParser.json())
+
+// ROUTERS 
+const userRouter = require('./routes/users')
 
 // Setup Database
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -27,10 +31,10 @@ db.on('error', err => console.log(err))
 db.once('open', () => console.log(`Connecting To ${process.env.DATABASE_URL} DB `))
 
 // Handel Routers
-app.use('/', (req, res) => res.send('Root Route'))
-app.use('/product', (req, res) => res.send('Product Route'))
-app.use('/user', (req, res) => res.send('User Route'))
-app.use('/categorie', (req, res) => res.send('Categorie Route'))
+//app.use('/', (req, res) => res.send('Root Route'))
+app.use('/products', (req, res) => res.send('Product Route'))
+app.use('/users', userRouter)
+app.use('/categories', (req, res) => res.send('Categorie Route'))
 app.use('*', (req, res) => res.send('There\'s No Route'))
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server Running...`))
