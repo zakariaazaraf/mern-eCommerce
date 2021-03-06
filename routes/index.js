873cfaeebf7120173af1756c697589c5a3289c53
zakaria ben salek <zakariaazaraf@gmail.com>
2021-03-06 20:44:27 +0100

    header on scroll

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Product = require('./../models/product')
 
-router.get('/', (req, res)=>{
-    res.render('index')
+router.get('/', async (req, res)=>{
+    try{
+        const products = await Product.find({}) 
+        res.render('index', {products: products})
+    }catch{
+        res.render('index')
+    }
 })
 
 module.exports = router
