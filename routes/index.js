f597a209f4e2622ed56b5dbbd3d383b9999bb454
zakaria ben salek <zakariaazaraf@gmail.com>
2021-03-09 00:12:28 +0100

    change the limit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Product = require('./../models/product')
 
 router.get('/', async (req, res)=>{
     try{
-        const products = await Product.find({}).sort({dateAdded: 'desc'}).limit(5)
+        const products = await Product.find({}).sort({dateAdded: 'desc'})
         res.render('index', {products: products})
     }catch{
         res.render('index')
