0abac9dddee1a827e5fdb1ac01706d2f243444eb
zakaria <zakariaazaraf@gmail.com>
2021-03-28 22:06:18 +0100

    comlate the shop logic and the router

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    res.render('shop')
+const Product = require('./../models/product')
+const Categorie = require('./../models/categorie')
+
+router.get('/', async (req, res)=>{
+    let products = []
+    try{
+         products = await Product.find({})
+         const categories = await Categorie.find({})
+         //products = await Product.findOne({_id: '6042436cb201a82594551022'}).populate('categorieId').exec()
+         
+        if(products.length > 0){
+            res.render('shop', {products, categories})
+            return;
+        }
+        res.render('shop', {products, categories})
+    }catch{
+        res.status(500).json({
+            message: 'Failed Getting Products'
+        })
+    }
 })
 
 module.exports = router
