9345cbf9379205df05c65991452de056f1a473c9
zakaria azaraf <zakariaazaraf@gmail.com>
2022-10-15 19:14:47 +0100

    added the shopping component and working to fetch the product and display the orders that has been already picked by the users

---
+++ client/src/components/Shopping.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react'
+
+export const Shopping = () => {
+    const [orders, serOrders] = useState([]);
+    const [products, setProducts] = useState([])
+
+    const getShoppingDate = async () => {
+        
+        try {
+            const response = await fetch(`http://localhost:5000/shopping`)
+
+            if (response.ok && response.status === 200) {
+                const data = await response.json();
+                let {orders, products} = data
+                console.log(orders, products)
+            }
+        } catch (error) {
+            /** Redirect the user to another page if something unexpected happend. Or even better, Redirect them to 404 page*/
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getShoppingDate()
+    }, [])
+    
+
+
+  return <div className="orders-container">
+  <div className="container">
+      <div className="card-orders">
+          {/* <%  let total = 0
+              orders.forEach(order=>{
+                  products.forEach(product=>{
+                      if(product.id == order.id){
+                          %>
+                              <div className="card-order" data-id='<%= product.id %>' data-price='<%= product.price %>'>
+                                  <div className="order-image">
+                                      <img src='/images/<%= product.coverImage%>' alt='<%=product.name%>'>
+                                  </div>
+                                  <div className="order-info">
+                                      <p className='product-name'>
+                                          <%=product.name%>
+                                      </p>
+                                      <p className='product-description'>
+                                          <%=product.description%>
+                                      </p>
+                                      <span className='quantity'>$<%=product.price%> x <%= product.quantity ? product.quantity : 1%></span>
+                                  </div>
+                                  <div className="order-cancel" title="remove order">
+                                      <span className='remove'></span>
+                                  </div>
+                              </div>
+                          <% 
+                          
+                          total += product.price
+                      } 
+                  }) 
+              }) 
+          %> */}
+      </div>
+      <div className="shipping">
+          <div className="checkout">
+              <div className="total">
+                  <span>totla :</span>
+                  {/* <span className='total-price'>$<%=total.toFixed(2)%></span> */}
+              </div>
+              <button className='btn'>checkout</button>
+          </div>
+          <div className="ship-info">
+              <div className='ship-info-content'>
+                  <div>Items are shipped within 24 hours by tracked DHL courier services</div>
+                  <div className='accept-holder'>
+                      <span>we accept :</span>
+                      <div className="img-icon-holder">
+                          <img src="images/masterCard.png" alt="mastercard" />
+                      </div>
+                      <div className="img-icon-holder">
+                          <img src="images/visa.png" alt="visa" />
+                      </div>
+                  </div>
+              </div>
+              <div className='ship-dealing'>
+                  <div className='deal-info'>
+                      <div className="img-icon-holder">
+                          <img src="images/shippingIcon.png" alt="cart" />
+                      </div>
+                      <p>Free Shipping Worldwide</p>
+                  </div>
+                  <div className='deal-info'>
+                      <div className="img-icon-holder">
+                          <img src="images/returns.png" alt="cart" />
+                      </div>
+                      <p>Free Returns Worldwide</p>
+                  </div>
+                  <div className='deal-info'>
+                      <div className="img-icon-holder">
+                          <img src="images/cart.png" alt="cart" />
+                      </div>
+                      <p>100% Safe & Secure</p>
+                  </div>
+                  
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+}
--- server/routes/shoppingCard.js
+++ server/routes/shoppingCard.js
@@ -6,19 +6,18 @@ const isAuth = require('./../middleware/is-auth')
 
 router.get('/'/* , isAuth */, async (req, res)=>{
     
-    let orders = req.cookies.orders != null ? req.cookies.orders : {}
+    // let orders = req.cookies.orders != null ? req.cookies.orders : {}
+    let orders = req.cookies.orders != null ? JSON.parse(req.cookies.orders) : {}
 
-    if(orders.length > 2){ // Weired Behviour
-        try{
-            const products = await Product.find({})
-            res.render('orders/order', {orders: JSON.parse(orders), products: products})
-    
-        }catch{
-            res.status(500).json({msg: 'Failed Getting Product'})
-        }
-        return
+    try{
+        const products = await Product.find({})
+        // res.render('orders/order', {orders: JSON.parse(orders), products: products})
+
+        res.status(200).json({ orders, products})
+
+    }catch{
+        res.status(500).json({msg: 'Failed Getting Product'})
     }
-    res.render('orders/emptyCard')
 
     
 }) 
